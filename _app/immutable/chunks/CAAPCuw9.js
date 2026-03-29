6a234a49590e05b5b913fc6655fbf1e5118efaa5
Endre Márk Borza <endremborza@gmail.com>
2026-03-30 01:01:26 +0200

    Updates

--- _app/immutable/chunks/CAAPCuw9.js
+++
@@ -1,1 +0,0 @@
-const s=globalThis.__sveltekit_1t3mza1?.base??"/swack",a=globalThis.__sveltekit_1t3mza1?.assets??s??"";export{a,s as b};
---
+++ _app/immutable/entry/start.C72dk88B.js
@@ -0,0 +1,1 @@
+import{l as o,a as r}from"../chunks/1-gpiHnc.js";export{o as load_css,r as start};
