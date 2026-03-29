36f091398c1113680c65b4d842cc7c01a9da1926
Endre Márk Borza <endremborza@gmail.com>
2026-03-29 23:07:37 +0200

    gh pages path

--- svelte.config.js
+++ svelte.config.js
@@ -4,7 +4,7 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	preprocess: vitePreprocess(),
-
+	paths: { base: process.env.NODE_ENV === 'production' ? '/swack' : '' },
 	kit: {
 		adapter: adapter({ fallback: '404.html' })
 	}
